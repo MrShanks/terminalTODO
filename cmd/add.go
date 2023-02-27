9b37647257a9f9f90c1e38f6edad8547cb387d42
MrShanks <simonestaffoli@gmail.com>
2023-02-27 16:32:39 +0100

    feat: add new task functionality.

--- cmd/add.go
+++ cmd/add.go
@@ -4,10 +4,13 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
+	"encoding/json"
+	"io"
 	"log"
 	"os"
+	"strings"
 
+	task "github.com/MrShanks/terminalTODO/pkg"
 	"github.com/spf13/cobra"
 )
 
@@ -15,7 +18,7 @@ var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Create new task",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+		add(args)
 	},
 }
 
@@ -23,11 +26,34 @@ func init() {
 	rootCmd.AddCommand(addCmd)
 }
 
-func add() {
+func add(input []string) {
 	tasksFile, err := os.Open("./tasks.json")
 	if err != nil {
 		log.Fatalf("Couldn't open the file: %v", err)
 	}
 	defer tasksFile.Close()
 
+	byteValue, err := io.ReadAll(tasksFile)
+	if err != nil {
+		log.Fatalf("Couldn't convert file into a bytes: %v", err)
+	}
+
+	var tasks task.Tasks
+
+	json.Unmarshal(byteValue, &tasks)
+
+	name := strings.Join(input, " ")
+
+	task := task.Task{
+		Name: name,
+	}
+
+	tasks.Tasks = append(tasks.Tasks, task)
+
+	jsonTasks, err := json.Marshal(tasks)
+	if err != nil {
+		log.Fatalf("Couldn't marshal tasks: %v", err)
+	}
+
+	os.WriteFile("./tasks.json", jsonTasks, 0644)
 }
--- cmd/list.go
+++ cmd/list.go
@@ -10,7 +10,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/MrShanks/terminalTODO/pkg/task"
+	task "github.com/MrShanks/terminalTODO/pkg"
 	"github.com/spf13/cobra"
 )
 
